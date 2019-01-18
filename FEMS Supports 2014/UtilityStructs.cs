ee83acc1461474b04e6b68005c8ce569337939e9
Dickins John <dickinsjohn@outlook.com>
2019-01-18 14:05:05 +0530

    Initial Commit

---
+++ FEMS Supports 2014/UtilityStructs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FEMS_Supports
+{
+    //structure to store specification data from file
+    public struct specificationData
+    {
+        public string selectedFamily;
+        public string discipline;
+        public double offset;
+        public double minSpacing;
+        public string supportType;
+        public string specsFile;
+    };
+
+    //specs sruct for FEMS team
+    public struct FEMS_Specs
+    {
+        public double dia;
+        public double spacing;
+    };
+}
